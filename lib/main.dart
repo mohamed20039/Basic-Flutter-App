ef3d95d7c4343f1881adf9cb054be48a9c5739c5
Mohamed <mohammedmohamuud.01@gmail.com>
2024-02-23 21:03:09 +0300

    ListTile Widget

--- lib/main.dart
+++ lib/main.dart
@@ -14,41 +14,19 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.yellow,
         title: Center(child: Text("My App")),
       ),
-      body: Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
-        Expanded(
-          flex: 1,
-          child: Container(
-            child: Text("Green"),
-            alignment: Alignment.center,
-            color: Colors.green,
-            width: 100,
-            padding: EdgeInsets.all(12.0),
-            height: 100,
-          ),
-        ),
-        Expanded(
-          flex: 3,
-          child: Container(
-            child: Text("cyan"),
-            alignment: Alignment.center,
-            color: Colors.cyan,
-            width: 100,
-            padding: EdgeInsets.all(12.0),
-            height: 100,
-          ),
-        ),
-        Expanded(
-          flex: 1,
-          child: Container(
-            child: Text("red"),
-            alignment: Alignment.center,
-            color: Colors.red,
-            width: 100,
-            padding: EdgeInsets.all(12.0),
-            height: 100,
-          ),
-        )
-      ]),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ListTile(
+            onTap: () => {print("Hello World")},
+            leading: Icon(Icons.image),
+            title: Text("Mohamed Mohamud"),
+            subtitle: Text("Full Stack Developer"),
+            trailing: Icon(Icons.call),
+          )
+        ],
+      )),
     );
   }
 }
