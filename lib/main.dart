1fa8604e78f594a226776b88e619b46a5857ac08
Mohamed <mohammedmohamuud.01@gmail.com>
2024-02-23 20:54:00 +0300

    Expanded Widget

--- lib/main.dart
+++ lib/main.dart
@@ -14,18 +14,41 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.yellow,
         title: Center(child: Text("My App")),
       ),
-      body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Wrap(
-            children: [
-              Text(
-                  "Hello world! Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!")
-            ],
-          )
-        ],
-      )),
+      body: Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+        Expanded(
+          flex: 1,
+          child: Container(
+            child: Text("Green"),
+            alignment: Alignment.center,
+            color: Colors.green,
+            width: 100,
+            padding: EdgeInsets.all(12.0),
+            height: 100,
+          ),
+        ),
+        Expanded(
+          flex: 3,
+          child: Container(
+            child: Text("cyan"),
+            alignment: Alignment.center,
+            color: Colors.cyan,
+            width: 100,
+            padding: EdgeInsets.all(12.0),
+            height: 100,
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child: Container(
+            child: Text("red"),
+            alignment: Alignment.center,
+            color: Colors.red,
+            width: 100,
+            padding: EdgeInsets.all(12.0),
+            height: 100,
+          ),
+        )
+      ]),
     );
   }
 }
